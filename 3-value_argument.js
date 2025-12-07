4d8a0a71d0f260f6ce9a76ab4d13124594367cab
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-12-07 18:53:50 +0300

    feat: task3 file - print 1st argv

---
+++ 3-value_argument.js
@@ -0,0 +1,14 @@
+//Script that prints the first argument passed to it
+const argvPassed = require('node:process'); //import process module
+const argv = argvPassed.argv; //import the arguments vector array from process module
+
+//print array element using array loop
+argv.forEach(value => {
+    if (value == '') {
+        console.log('No argument');
+    }
+    else      
+        {
+        console.log(value);
+    }
+})
