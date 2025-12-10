41f38ad9680c59470e25b39ee50b983ea72fed02
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-12-10 05:34:59 +0300

    fix:node typecasting function parseInt()

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,8 @@
 //Script that prints My number: <first argument converted in integer>
 const {argv} = require('node:process'); //destructuring assignment
-const { isNumberObject } = require('node:util/types');
+
 const firstUserArgument = argv[2];
+
 if (firstUserArgument === undefined) {
     console.log('Not a number');
 }
