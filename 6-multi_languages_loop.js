0315b0898aa6eff3329a611b8e4fdfdef329aef8
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-12-10 05:54:52 +0300

    feat: task6 file do while loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+//Script that prints 3 lines but by using an array of string and a loop
+
+let elements = [1, 2, 3]; //anchor for looping
+let i = 0;
+
+do {
+    console.log('C is fun');
+    console.log('Python is cool');
+    console.log('JavaScript is amazing');
+}
+while (i > elements.length)
