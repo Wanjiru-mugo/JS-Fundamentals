2324f1b95c373d318ef137eed81386af6f0d7250
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-12-10 04:20:28 +0300

    fix:white space

--- 4-concat.js
+++ 4-concat.js
@@ -1,5 +1,5 @@
 //Script that prints two arguments passed to it, in the following format: “ is ”
-const {argv} = require('node: process');
+const {argv} = require('node:process');
 //assigning 1st and 2nd arguments for ID
 const firstUserArgument = argv[2]; 
 const secondUserArgument = argv[3];
