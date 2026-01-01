8774668b53d2b2b76251380ebbd784ef7b655525
Wanjiru-mugo <wanjirumugomary@gmail.com>
2026-01-02 01:15:48 +0300

    fix: call function on parsed numbers

--- 9-add.js
+++ 9-add.js
@@ -4,6 +4,10 @@ const {argv} = require('node:process');
 const firstUserArgument = argv[2];
 const secondUserArgument = argv[3];
 
+function add(a, b) {
+        return a + b;
+    }
+
 //lets convert the arguments parssed into int
 const a = parseInt(firstUserArgument, 10);
 const b = parseInt(secondUserArgument, 10);
@@ -13,8 +17,6 @@ if (isNaN(a) || isNaN(b)) {
     console.log('NaN');
 }
 else {
-    function add(a, b) {
-        console.log(a + b);
-    }
-
+    let result = add(a, b);
+    console.log(result);
 }
