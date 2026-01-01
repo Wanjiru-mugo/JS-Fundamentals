2ff057a51cd009df522098234f10e5448fed8bd1
Wanjiru-mugo <wanjirumugomary@gmail.com>
2026-01-02 02:36:34 +0300

    feat: task10 file recursive argument function

---
+++ 10-factorial.js
@@ -0,0 +1,22 @@
+//script that computes and prints a factorial
+//fetch argv from process module
+const {argv} = require('node:process');
+firstUserArgument = argv[2];
+
+const factorial = a => {
+    if (a <= 1) {
+        return 1;
+    } 
+    else {
+        return a * factorial(a - 1);
+    }
+}
+//typecasting the argument into a number
+const a = parseInt(firstUserArgument, 10);
+if (isNaN(a, 10)) {
+    console.log(1);
+}
+else {
+    result = factorial(a);
+    console.log(result);
+}
